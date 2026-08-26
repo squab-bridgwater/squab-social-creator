214d9ab47d3062ff5790e48a80ff9c39f23436a4
squab-bridgwater <squabbridgwater@gmail.com>
2026-08-26 16:49:49 +0100

    Make V2 build GitHub Pages path-safe

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: 'source',
+  base: './',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
