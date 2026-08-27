3dc40541736742fff51e64bd6be7708f2eb47538
squab-bridgwater <squabbridgwater@gmail.com>
2026-08-27 14:36:30 +0100

    Add guided prompt matrix route

--- source/main.tsx
+++ source/main.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { ReviewLab } from './ReviewLab';
+import { PromptWizard } from './guided/PromptWizard';
 import './styles.css';
 import './v2-additions.css';
 import './art-directions.css';
 import './review-lab.css';
+import './guided/guided.css';
 
-const isReviewLab = new URLSearchParams(window.location.search).get('lab') === '1';
+const params = new URLSearchParams(window.location.search);
+const isReviewLab = params.get('lab') === '1';
+const isGuidedWizard = params.get('wizard') === '1';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {isReviewLab ? <ReviewLab /> : <App />}
+    {isGuidedWizard ? <PromptWizard /> : isReviewLab ? <ReviewLab /> : <App />}
   </React.StrictMode>,
 );
