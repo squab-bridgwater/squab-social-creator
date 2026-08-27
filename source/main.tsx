577ee613e2c06732eff906a73b5a187694430065
squab-bridgwater <squabbridgwater@gmail.com>
2026-08-27 13:45:38 +0100

    Add development visual review lab route

--- source/main.tsx
+++ source/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { ReviewLab } from './ReviewLab';
 import './styles.css';
 import './v2-additions.css';
 import './art-directions.css';
+import './review-lab.css';
+
+const isReviewLab = new URLSearchParams(window.location.search).get('lab') === '1';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isReviewLab ? <ReviewLab /> : <App />}
   </React.StrictMode>,
 );
